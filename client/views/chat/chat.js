d66ca6e403258b7941bab062f1026300c8df1305
Darren Buckner <darren+git@darrenbuckner.com>
2014-04-26 19:43:48 -0700

    added scroll to

--- client/views/chat/chat.js
+++ client/views/chat/chat.js
@@ -1,6 +1,6 @@
 Template['chat'].helpers({
   chatroom: function(){
-    return Session.get("name")
+    return Session.get("name");
   }
 });
 
@@ -10,16 +10,20 @@ Template['chat'].events({
     if (event.which == 13) { // 13 is the enter key event
       var name = 'Anonymous';
       var message = document.getElementById('message');
-      if (message.value != '') {
+      if (message.value !== '') {
         Chat.update({_id: Session.get("name")}, {$push:{messages:{message: message.value}}});
         document.getElementById('message').value = '';
         message.value = '';
       }
+      var ourLastChatRecord = $('.msg-container').last();
+      $('.messages-container').animate({scrollTop: $('.msg-container').last().offset().top});
+      // ourLastChatRecord.scrollTo(ourRecordPostion);
+      console.log($('.msg-container').last());
+      console.log($('.msg-container').length);
     }
   }
 });
 
 Template.chat.rendered = function () {
 
-}
-
+};
