e6ae3aafaef80c0bea1bff731e95bd103cb5f5f3
David Ordal <david@ordal.com>
2014-04-27 10:30:58 -0700

    refactor chat.js; add send button functionality

--- client/views/chat/chat.js
+++ client/views/chat/chat.js
@@ -1,67 +1,82 @@
 Template['chat'].helpers({
-  chatroom: function(){
-    return Session.get("name")
-  },
-  nickname: function(){
-    return Session.get('nickname') || 'Anonymous'
-  },
-  prettyTime: function(datetime){
-    if(typeof datetime === 'object'){
-      var minutes = (datetime.getMinutes()<10 ?'0':'') + datetime.getMinutes()
-      return "" + datetime.getHours() + ":" + minutes
-      }
-    else{
-      return datetime
+    chatroom: function(){
+        return Session.get("name")
+    },
+    nickname: function(){
+        return Session.get('nickname') || 'Anonymous'
+    },
+    prettyTime: function(datetime){
+        if(typeof datetime === 'object'){
+            var minutes = (datetime.getMinutes()<10 ?'0':'') + datetime.getMinutes()
+            return "" + datetime.getHours() + ":" + minutes
+            }
+        else{
+            return datetime
+        }
     }
-  }
 });
 
-
 Template['chat'].events({
-  'keydown #message' : function (event) {
-    if (event.which == 13) { // 13 is the enter key event
-      nickField = document.getElementById('nickname')
-      if (nickField.value != '') {
-        Session.set('nickname', nickField.value)
-      } else {
-        nickField.value = 'Anonymous'
-      }
-      var name = Session.get('nickname') || document.getElementById('nickname').value ;
-      var message = document.getElementById('message');
-      if (message.value != '') {
-        Chat.update({_id: Session.get("id")}, {$push:{messages:{name: name, message: message.value, time: new Date}}});
-        document.getElementById('message').value = '';
-        message.value = '';
-        var ourLastChatRecord = $('.msg-container').last();
-        // $('.messages-container').animate();
-        // console.log($('.messages-container').height());
-        // $(".messages-container").scrollTop($('.messages-container').attr("scrollHeight"));
-        // var messageContainer = $('.messages-container');
-        // messageContainer.scrollTop(messageContainer.prop("scrollHeight"));
+    // send message
+    'keydown #message' : function (event) {
+        if (event.which == 13) { // 13 is the enter key event
+            Template.chat.extras.enterMessage();
+        }
+    },
 
-      }
+    'click #send-btn' : function (event) {
+        Template.chat.extras.enterMessage();
+    },
+
+    // change nickname
+    'keydown #nickname' : function (event) {
+        if (event.which == 13 ) {
+            Template.chat.extras.updateNickname();
+        }
+    },
+    'blur #nickname' : function (event) {
+        Template.chat.extras.updateNickname();
     }
-  },
-  'keydown #nickname' : function (event) {
-    if (event.which == 13 ) {
-      var nickname = $('#nickname').val();
-      Session.set("nickname", nickname);
-      ReactiveStore.set("clink-nickname", nickname);
-    }
-  },
-  'blur #nickname' : function (event) {
-    Session.set("nickname", document.getElementById('nickname').value || 'Anonymous');
-  }
 });
 
 Template.chat.rendered = function () {
+    // set the nickname, if not set
+    var nickname = Session.get("nickname");
+    if (typeof nickname == 'undefined') {
+        $('#nickname').val('Anonymous');
+        Session.set("nickname", nickname);
+    }
+
+    // resize
      $(window).resize(function(){
-      var ourWindowHeight = $(window).height();
-      $('.messages-container').css("height", ourWindowHeight);
+        var ourWindowHeight = $(window).height();
+        $('.messages-container').css("height", ourWindowHeight);
     });
     $('#message').focus();
     $('.toggle-name-input').click(function(e) {
-      e.preventDefault();
-      $('.chat-options').toggleClass('hide');
+        e.preventDefault();
+        $('.chat-options').toggleClass('hide');
     });
 }
+
+Template.chat.extras = {
+    updateNickname: function() {
+        var nickname = $('#nickname').val();
+        Session.set("nickname", nickname);
+        ReactiveStore.set("clink-nickname", nickname);
+    },
+    enterMessage: function() {
+        var nickname = Session.get("nickname");
+        var message = $('#message').val();
+        if (message != '') {
+            Chat.update({_id: Session.get("id")}, {$push:{messages:{name: nickname, message: message, time: new Date}}});
+            $('#message').val('');  // reset the message
+            var ourLastChatRecord = $('.msg-container').last();
+            // $('.messages-container').animate();
+            // console.log($('.messages-container').height());
+            // $(".messages-container").scrollTop($('.messages-container').attr("scrollHeight"));
+            // var messageContainer = $('.messages-container');
+            // messageContainer.scrollTop(messageContainer.prop("scrollHeight"));
+        }
+    }
+};
