5ef81c68f2429197bce7f0d6effb4ea5db525aa9
Darren Buckner <darren+git@darrenbuckner.com>
2014-04-26 18:45:12 -0700

    homepage updates

--- client/views/hp/hp.js
+++ client/views/hp/hp.js
@@ -2,5 +2,10 @@ Template['hp'].helpers({
 });
 
 Template['hp'].events({
+	'click .expose-share-url': function(e) {
+		$('.expose-share-url').fadeOut('fast', function() {
+			$('.share-url-wrap').removeClass('hide').fadeIn('fast');
+		});
+        // $('.share-url-wrap').removeClass('hide').fadeIn('fast');
+    }
 });
-
